d5ba04f9e68ebf9655c2df8b00d90eea264d90d0
Tarick Ahmed <newstartin702@gmail.com>
2024-07-28 18:12:00 -0700

    test commmit

---
+++ index.js
@@ -0,0 +1,88 @@
+const=[3, 9, 23, 64, 2, 8, 28, 93.]{;
+    const firstElement = ages[0];
+    const lastElement = ages.at(-1);
+
+}
+Array.push (44)
+const result = lastElement - firstElement;
+
+(lastElement=0,lastElement<firstElement,lastElement++)
+
+
+
+//2
+const names=['Sam', 'Tommy', 'Tim', 'Sally', 'Buck', 'Bob'.]
+let totalLetters = 0;
+for (let i = 0; i < names.length; i++) {
+    totalLetters += names[i].length;
+    const averageLetters = totalLetters / names.length; 
+console.log(averageLetters);}
+
+//3
+
+const lastOne=array.at(-1)
+
+//4
+
+const firstOne=array[0]
+
+//5
+
+nameLengths=[]
+let addOns=[Kelly, Sam, Kate]
+(let i = 0; i < nameLengths; i++) {{
+}
+    nameLengths.push(names[i].length);}
+
+//6
+let sum=0
+for (let i = 0; i < nameLengths.length; i++) {
+    totalSum += nameLengths[i];
+    console.log (totalSum)
+
+//7
+
+function repeat(word, n) {
+    let result = ''; // 
+    for (let i = 0; i < n; i++) {
+        result += word; 
+    }
+    return result
+}
+//8
+
+function fullName= (Firstname + Lastname)
+console.log (`FirstName +  +Lastname`)
+
+//9
+
+function sumisGreater() {
+    const sum = numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sum > 100;
+
+//10
+function average(numbers) {
+    if (numbers.length === 0) {
+            }
+
+}    
+
+//11
+function isAverageGreater(array1, array2) {
+    function average(numbers) {
+        if (numbers.length === 0) {
+             new Error("Array cannot be empty.");
+        }
+        const totalSum = numbers.reduce((acc, num) => acc + num, 0);
+        return totalSum / numbers.length;
+//12
+function buyDrink(isHotOutside, moneyInPocket) {
+    return isHotOutside && moneyInPocket > 10.50;
+
+ //13
+ 
+ function shouldBuyGas(pricePerGallon) {
+    return pricePerGallon < 4.00;
+}//I created this function because gas prices are out of this world, and a little tool would help.
+}
+
